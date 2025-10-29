6790764311ce6c33d3052922789c44b2e65fe1a5
rprasad22 <rajneel1@ualberta.ca>
2025-10-29 17:24:56 -0600

    Successful test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomListTest {
+
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
